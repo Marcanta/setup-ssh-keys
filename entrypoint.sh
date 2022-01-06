e0490767315c9e53abdbcf302262a31860616210
Marcant Aimé <47029620+Marcanta@users.noreply.github.com>
2022-01-06 17:24:53 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ params=$1
 jq -r 'to_entries | map(.value+{name: .key}) | map([.user, .port, .ip, .password, .name]) | .[] | @tsv' <<< $params |
   while IFS=$'\t' read -r user port ip password name; do
     echo "processing $name..."
-    sshpass -p $password ssh -o StrictHostKeyChecking=no -o ConnectTimeout=30 -p $port $user@$ip "mkdir ~/.ssh"
+#     sshpass -p $password ssh -o StrictHostKeyChecking=no -o ConnectTimeout=30 -p $port $user@$ip "mkdir ~/.ssh"
     sshpass -p $password scp -o StrictHostKeyChecking=no -o ConnectTimeout=30 -pr -P $port authorized_keys $user@$ip:~/.ssh/authorized_keys
   done
 
