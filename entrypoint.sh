35602a677e160516dda74b02df55fc36876dd8f5
Marcant Aimé <47029620+Marcanta@users.noreply.github.com>
2022-01-06 10:36:33 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,8 +6,8 @@ authorized_keys=$2
 jq -r 'to_entries | map(.value+{name: .key}) | map([.user, .port, .ip, .password, .name]) | .[] | @tsv' <<< $params |
   while IFS=$'\t' read -r user port ip password name; do
     echo "$port and $ip and $password and $name and $user"
-    sshpass -p "$password" ssh -p $port $user@$ip "mkdir .ssh"
-    sshpass -p "$password" scp -pr -P $port authorized_keys $user@$ip:~/.ssh/authorized_keys
+    sshpass -p "$password" ssh -o ConnectTimeout=30 -p $port $user@$ip "mkdir .ssh"
+    sshpass -p "$password" scp -o ConnectTimeout=30 -pr -P $port authorized_keys $user@$ip:~/.ssh/authorized_keys
   done
 
 time=$(date)
