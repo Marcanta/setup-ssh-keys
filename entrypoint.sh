da8c41fda980c4395d44aef2ffe72e26484dc9d6
Marcant Aimé <47029620+Marcanta@users.noreply.github.com>
2022-01-06 11:46:58 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,17 +2,15 @@
 
 params=$1
 
-# jq -r 'to_entries | map(.value+{name: .key}) | map([.user, .port, .ip, .password, .name]) | .[] | @tsv' <<< $params |
-#   while IFS=$'\t' read -r user port ip password name; do
-#     echo "processing $name...\n"
-#     echo 'sshpass -p ${password} ssh -v -p ${port} ${user}@${ip} "mkdir ~/.ssh"'
-#     sshpass -p ${password} ssh -v -p ${port} ${user}@${ip} "mkdir ~/.ssh"
-#     echo "\n 2nd command, home: $HOME \n"
-#     echo 'sshpass -p $password scp -prv -P $port authorized_keys $user@$ip:~/.ssh/authorized_keys'
-#     sshpass -p $password scp -prv -P $port authorized_keys $user@$ip:~/.ssh/authorized_keys
-#   done
-sshpass -p "kkl98YHD56fedgh7654" ssh -o ConnectTimeout=30 -v -p 12749 tabmin@cataloguetest.tabuleo.com "mkdir ~/.ssh"
-sshpass -p "kkl98YHD56fedgh7654" scp -o ConnectTimeout=30 -pr -P 12749 authorized_keys tabmin@cataloguetest.tabuleo.com:~/.ssh/authorized_keys
+jq -r 'to_entries | map(.value+{name: .key}) | map([.user, .port, .ip, .password, .name]) | .[] | @tsv' <<< $params |
+  while IFS=$'\t' read -r user port ip password name; do
+    echo "processing $name...\n"
+    echo 'sshpass -p $password ssh -o StrictHostKeyChecking=no -o ConnectTimeout=30 -p $port $user@$ip "mkdir ~/.ssh"'
+    sshpass -p $password ssh -v -p $port $user@$ip "mkdir ~/.ssh"
+    echo "\n 2nd command, home: $HOME \n"
+    echo 'sshpass -p $password scp -prv -P $port authorized_keys $user@$ip:~/.ssh/authorized_keys'
+    sshpass -p $password scp -o StrictHostKeyChecking=no -o ConnectTimeout=30 -pr -P $port authorized_keys $user@$ip:~/.ssh/authorized_keys
+  done
 
 time=$(date)
 
